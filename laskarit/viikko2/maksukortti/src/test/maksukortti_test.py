683bc904dfe968932407881e14f807f9099fcd70
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-12 15:48:12 +0200

    Add tests for exercise 3

--- laskarit/viikko2/maksukortti/src/test/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/test/maksukortti_test.py
@@ -24,12 +24,36 @@ class TestMaksukortti(unittest.TestCase):
 
         self.assertEqual(kortti.saldo_euroina(), 2.0)
     
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(300)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 3.0)
+    
     def test_kortille_voi_ladata_rahaa(self):
         self.kortti.lataa_rahaa(2500)
 
         self.assertEqual(self.kortti.saldo_euroina(), 35.0)
 
+    def test_negatiivinen_summa_ei_muuta_saldoa(self):
+        self.kortti.lataa_rahaa(-1000)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 10)
+
     def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
         self.kortti.lataa_rahaa(20000)
 
         self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+
+    def test_edullisen_lounaan_saldo(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+    
+    def test_maukkaan_lounaan_saldo(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+    
