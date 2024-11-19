1c0224f6f53f28590c6b5615041c98c1cb1bbcfd
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-19 21:42:03 +0200

    Add Invoke start task

--- src/index.py
+++ src/index.py
@@ -5,5 +5,3 @@ from ui.ui import UI
 def main():
     window = tk.Tk()
     window.title("Budgeting App")
-
-    
--- tasks.py
+++ tasks.py
@@ -2,6 +2,10 @@ from invoke import task
 
 
 @task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+@task
 def build(ctx):
     ctx.run("python3 src/build.py", pty=True)
 
