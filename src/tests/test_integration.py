25ec6fd20461e2042e5907258ed7974400f1a504
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-12-22 17:15:41 +0200

    Add integration tests for log in and logout, update .coveragerc

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -5,13 +5,15 @@ from repositories.budget_repository import budget_repository
 from repositories.user_repository import user_repository
 from entities.user import User
 from entities.budget import Budget
+from utils.password_utils import hash_password
 
 
 class TestIntegration(unittest.TestCase):
     def setUp(self):
         budget_service.delete_all()
         user_repository.delete_all()
-        self.user_test1 = User('test_user', 'password123')
+        hashed_password = hash_password('password123')
+        self.user_test1 = User('test_user', hashed_password)
         user_repository.create(self.user_test1)
         self.budget_test1 = Budget(
             'test_user', 10, 'Expense', '22.12.2024', 'example1')
@@ -28,6 +30,18 @@ class TestIntegration(unittest.TestCase):
         new_user = user_service.create_user(username, password)
 
         self.assertEqual(new_user, user_service.get_current_user())
+
+    def test_user_log_in(self):
+        user_service.login('test_user', 'password123')
+
+        self.assertEqual(self.user_test1.username,
+                         user_service.get_current_user().username)
+    
+    def test_user_log_out(self):
+        user_service.login('test_user', 'password123')
+        user_service.logout()
+
+        self.assertEqual(None, user_service.get_current_user())
 
     def test_create_existing_user_raises_error(self):
         username = "test_user"
@@ -66,14 +80,14 @@ class TestIntegration(unittest.TestCase):
         expected_budgets = [self.budget_test1, self.budget_test2]
 
         self.assertEqual(budgets, expected_budgets)
-    
+
     def test_delete_budget(self):
         created_budget = budget_service.create_budget(self.user_test1,
                                                       self.budget_test1.amount,
                                                       self.budget_test1.category,
                                                       self.budget_test1.date,
                                                       self.budget_test1.tag)
-        
+
         budget_service.delete_budget_by_id(created_budget.id)
 
         budgets = budget_service.get_user_budgets(self.user_test1)
